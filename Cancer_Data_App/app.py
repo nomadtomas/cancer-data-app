691f220d7b88ddd28f07fe292c2f56daf8155192
Tomas Resendiz <l2tt2lresendiz@gmail.com>
2019-05-25 20:48:38 -0500

    upadated app.py

--- Cancer_Data_App/app.py
+++ Cancer_Data_App/app.py
@@ -19,7 +19,9 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db/bellybutton.sqlite"
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
